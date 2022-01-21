420f94ca0b956308fe59c43eec8381f600de283a
Rogerio Siqueira <rosf@gft.com>
2022-01-21 10:58:38 -0300

    Teste com erro no código

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -4,6 +4,6 @@ describe('home page', () => {
     it('app deve estar online', ()=> {
         
         cy.visit('/')
-        cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
+        cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eati')
     })
 })
